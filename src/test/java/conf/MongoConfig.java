da3ea9f51beb171132aac49ae07fd9374c976f6e
lu.dengji <lu.dengji@gmail.com>
2017-03-11 12:47:28 +0800

    调整mongo的配置

--- src/test/java/conf/MongoConfig.java
+++ src/test/java/conf/MongoConfig.java
@@ -1,7 +1,36 @@
 package conf;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
 /**
  * Created by meisei on 2017/3/11.
  */
+@Configuration
 public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongodbUri;
+
+    @Value("${spring.data.mongodb.dbName}")
+    private String dbName;
+
+    @Bean("mongoTemplate")
+    public MongoTemplate mongoTemplate() {
+        Mongo mongoClient = new MongoClient(new MongoClientURI(mongodbUri, new MongoClientOptions.Builder()
+                .connectionsPerHost(20)
+                .minConnectionsPerHost(20)
+                .connectTimeout(20)
+                .maxWaitTime(20)
+                .socketKeepAlive(true)
+                .socketTimeout(20)));
+        return new MongoTemplate(mongoClient, dbName);
+    }
 }
